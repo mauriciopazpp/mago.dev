364e1a133bfddc040e5b37f509596b7893c585bc
Mauricio Paz <mauricio.pazpacheco@blueacornici.com>
2023-02-14 14:48:51 -0300

    First commit

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'mago.dev',
+    site_url: 'https://mago.dev',
+    feed_url: 'https://mago.dev/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
